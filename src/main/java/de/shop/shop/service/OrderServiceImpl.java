3105943ae201c0ed6f352a5141c340380f414d36
Alexander <alexander.fandre@stud.uni-bamberg.de>
2021-12-16 17:01:41 +0100

    current approach

--- src/main/java/de/shop/shop/service/OrderServiceImpl.java
+++ src/main/java/de/shop/shop/service/OrderServiceImpl.java
@@ -1,9 +1,19 @@
 package de.shop.shop.service;
 
+import de.shop.shop.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
-    
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    OrderServiceImpl(OrderRepository orderRepository){
+        this.orderRepository = orderRepository;
+    }
+
+
+
 }
