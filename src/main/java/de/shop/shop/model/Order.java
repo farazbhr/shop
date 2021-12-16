22d742e062eb334c997bf859f7ba1ae78d17051a
Alexander <alexander.fandre@stud.uni-bamberg.de>
2021-12-16 15:01:00 +0100

    current approach

--- src/main/java/de/shop/shop/model/Order.java
+++ src/main/java/de/shop/shop/model/Order.java
@@ -5,6 +5,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Entity()
+@Table(name="ShopOrder")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Positive(message="Price must be > 0")
+    private double price;
+
+    @OneToMany
+    @NotNull
+    @NotEmpty
+    private List<OrderItem> orderItemList;
+
 }
--- src/main/java/de/shop/shop/model/OrderItem.java
+++ src/main/java/de/shop/shop/model/OrderItem.java
@@ -1,4 +1,33 @@
 package de.shop.shop.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Positive(message="Price must be > 0")
+    private double price;
+
+    @Pattern(regexp = "^[0-9]*$", message = "Position can only digits")
+    private String position;
+
+    @NotNull
+    private Long beverageId;
 }
